bd9cc3dd62dedd97106e1b56bd50ea55654c5235
Jesse Beck <thewebbeckons@users.noreply.github.com>
2023-09-23 23:33:09 -0400

    fix coffee

--- nuxt.config.js
+++ nuxt.config.js
@@ -54,6 +54,6 @@ export default {
   // Modules (https://go.nuxtjs.dev/config-modules)
   modules: ['@nuxtjs/axios'],
 
-  // Build Configuration (https://go.nuxtjs.dev/config-build)
+  // Build Configuration (https://go.nuxtjs.dev5/config-build)
   build: {},
 }
